4176a2b7864c98db07bb7e8181ab3002d923ab34
gavrun <gavrunov@live.com>
2021-04-29 07:56:42 +0300

    added key motion events

--- Form1.cs
+++ Form1.cs
@@ -12,29 +12,23 @@ namespace platform_game
 {
     public partial class Form1 : Form
     {
-
+        //motion and progress
         bool goLeft, goRight, jumping, isGameOver;
-
         int jumpSpeed;
         int force;
         int score = 0;
         int playerSpeed = 7;
-
+        //platforms
         int horizontalSpeed = 5;
         int verticalSpeed = 3;
-
+        //enemies
         int enemyOneSpeed = 5;
         int enemyTwoSpeed = 3;
-
-
 
         public Form1()
         {
             InitializeComponent();
         }
-
-
-
 
         private void MainGameTimerEvent(object sender, EventArgs e)
         {
@@ -43,52 +37,66 @@ namespace platform_game
 
         private void KeyIsDown(object sender, KeyEventArgs e)
         {
-
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+            if (e.KeyCode == Keys.Space && jumping == false)
+            {
+                jumping = true;
+            }
         }
 
         private void KeyIsUp(object sender, KeyEventArgs e)
         {
-
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (jumping == true)
+            {
+                jumping = false;
+            }
+            //option to reset while playing
+            if (e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                RestartGame();
+            }
         }
-
 
         private void RestartGame()
         {
             //reset motion and progress
-
             jumping = false;
             goLeft = false;
             goRight = false;
             isGameOver = false;
             score = 0;
-
             txtScore.Text = "Score: " + score;
-
             //reset coins
-
             foreach (Control x in this.Controls)
             {
                 if (x is PictureBox && x.Visible == false)
                 {
                     x.Visible == true;
                 }
-
             }
-
             //reset position of player, platforms, and enemies
-
             player.Left = 23;
             player.Top = 642;
-
             enemyOne.Left = 266;
             enemyTwo.Left = 280;
-
             horizontalPlatform.Left = 385;
             verticalPlatform.Top = 546;
-
             gameTimer.Start();
-
-
         }
     }
 }
